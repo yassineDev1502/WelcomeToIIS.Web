e01235bbce344f90590b873231a7648143c3ae43
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2026-05-12 15:13:24 +0200

    Initial commit

---
+++ WelcomeToIIS.Web/Program.cs
@@ -0,0 +1,38 @@
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//builder.Configuration.AddAzureKeyVault(
+//    new Uri("https://mywebappkeyvaluew.vault.azure.net/"),
+//    new DefaultAzureCredential());
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
